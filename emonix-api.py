28541370539727c593bf33830f08d61f4c5f29f8
Kyle <kyle@eatstreet.com>
2015-04-22 17:10:51 -0500

    add email service

--- emonix-api.py
+++ emonix-api.py
@@ -5,6 +5,7 @@ import falcon
 import json
 import requests
 import subprocess
+import smtplib
 
 fmt = lambda obj: json.dumps(obj, indent=4, sort_keys=True)
 
@@ -48,6 +49,38 @@ class closeHeatingValveResource:
         response['console_response'] = output
         resp.body = fmt(response)
         return response
+class emailResource:
+    def on_get(self, req, resp, message, email, name, subject ):
+        """Handles GET requests"""
+        resp.status = falcon.HTTP_200
+        resp.set_header('Access-Control-Allow-Origin', '*')
+        try:
+            
+            s=smtplib.SMTP()
+            s.connect("smtp.gmail.com",587)
+            s.starttls()
+            s.login("wirelessdigitalthings@gmail.com", "YOURMOM.COM")
+            FROM = str(email)
+            TO = ["wirelessdigitalthings@gmail.com"]
+            headers = "\r\n".join(["from: " + email,
+                       "subject: " + subject,
+                       "to: wirelessdigitalthings@gmail.com",
+                       "mime-version: 1.0",
+                       "content-type: text/html"])
+            body = 'name: ' + name + ' email:  ' + email + ' message: ' + message
+            # body_of_email can be plaintext or html!                    
+            content = headers + "\r\n\r\n" + body
+            print(content)
+            s.sendmail(FROM, TO, content)
+            s.quit()
+
+            return 'sent mail'
+
+        except:
+            exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+            print('exception %s was thrown' % exceptionValue)
+            return 'exception %s was thrown' % exceptionValue
+
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
@@ -55,10 +88,12 @@ app = falcon.API()
 # Resources are represented by long-lived class instances
 openValve = openHeatingValveResource()
 closeValve = closeHeatingValveResource()
+sendEmail = emailResource()
 
 # things will handle all requests to the '/things' URL path
 app.add_route('/valve/open', openValve)
 app.add_route('/valve/close', closeValve)
+app.add_route('/email/{message}/from/{email}/name/{name}/sub/{subject}', sendEmail)
 
 if __name__ == '__main__':
     # For testing outside a WSGI like gunicorn
